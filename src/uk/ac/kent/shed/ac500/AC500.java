b9b5f442e623fc4bc6089c3712e5afd5d86944d4
mjrb5 <berry120@gmail.com>
2015-01-07 16:22:35 +0000

    Add ability to start a reception attempt

--- src/uk/ac/kent/shed/ac500/AC500.java
+++ src/uk/ac/kent/shed/ac500/AC500.java
@@ -23,39 +23,56 @@ public class AC500 {
     }
 
     public TimeInfo getTime() {
+        byte[] arr = getBytes("o", 16);
+        return new TimeInfo(arr);
+    }
+    
+    public void startReceptionAttempt() {
+        getBytes("h", 0);
+    }
+
+    private byte[] getBytes(String strToSend, int numBytes) {
         try {
             List<Byte> bytesList = new ArrayList<>();
             SerialPort serialPort = new SerialPort(port);
             serialPort.openPort();
             serialPort.setParams(300, 7, 2, SerialPort.PARITY_EVEN);
-            serialPort.writeString("o");
+            serialPort.writeString(strToSend);
             Thread.sleep(200);
             serialPort.writeString("\r");
             Thread.sleep(200);
-            serialPort.readBytes(2); //Swallow echoed chars
-            while (bytesList.size() < 16) {
+            serialPort.readBytes(strToSend.length() + 1); //Swallow echoed chars
+            while (bytesList.size() < numBytes) {
                 byte[] bytes = serialPort.readBytes();
                 if (bytes == null) {
                     continue;
                 }
                 for (byte b : bytes) {
                     bytesList.add(b);
-                    if (bytesList.size() == 16) {
+                    if (bytesList.size() >= numBytes) {
                         break;
                     }
                 }
             }
-            return new TimeInfo(bytesList.toArray(new Byte[bytesList.size()]));
+            serialPort.closePort();
+            byte[] ret = new byte[bytesList.size()];
+            for (int i = 0; i < ret.length; i++) {
+                ret[i] = bytesList.get(i);
+            }
+            return ret;
         } catch (SerialPortException | InterruptedException ex) {
             throw new IllegalStateException("Couldn't get time from serial port", ex);
         }
     }
 
     /**
+     * Just testing
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         AC500 ac500 = new AC500("COM2");
+        System.out.println(ac500.getTime());
+        ac500.startReceptionAttempt();
         System.out.println(ac500.getTime());
     }
 
